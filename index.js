eda5e629baead288b2ffac99544dcb5a65bc1bac
sakamotot <sakamotot@intellilink.co.jp>
2024-03-26 15:22:29 +0900

    add systeminformation

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const si = require('systeminformation');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+  si.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
 });
 
 const PORT = process.env.PORT || 3000;
